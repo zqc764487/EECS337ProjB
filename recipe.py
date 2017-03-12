676699923a08c11ff8a57a7d70ac4da5fd252890
Steven <stevenzmanx@u.northwestern.edu>
2017-03-11 20:38:30 -0600

    fixed globals

--- recipe.py
+++ recipe.py
@@ -1,5 +1,11 @@
 import urllib
 import re
+caps = "([A-Z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov)"
 import nltk
 from bs4 import BeautifulSoup
 from collections import Counter, defaultdict
@@ -560,6 +566,7 @@ def convertCuisine(recipe, toType):
     return substitutes
 def intersect(a, b):
     return list(set(a) & set(b))
+
 def split_into_sentences(text):
     text = " " + text + "  "
     text = text.replace("\n"," ")
